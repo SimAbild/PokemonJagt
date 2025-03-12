d105404956b769c4b7b8464ca001bc53f242f517
SimAbild <abildtrup123@gmail.com>
2025-03-12 10:49:57 +0100

    Ændringer, har tilføjet extended og brugt inheritance

---
+++ src/Area.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Area {
+    private ArrayList<Pokemon> pokemonList = new ArrayList<>();
+
+    public void addPokemon(Pokemon pokemon) {
+        pokemonList.add(pokemon);
+    }
+
+    public void showAllPokemon() {
+        for (Pokemon p : pokemonList) {
+            System.out.println(p);
+        }
+    }
+
+    public Pokemon searchPokemonByName(String name) {
+        for (Pokemon p : pokemonList) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public Pokemon searchPokemonByType(String type) {
+        for (Pokemon p : pokemonList) {
+            if (p.getType().equalsIgnoreCase(type)) {
+                return p;
+            }
+        }
+        return null;
+    }
+}
--- src/StarterForest.java
+++ src/StarterForest.java
@@ -1,34 +1,10 @@
 import java.util.ArrayList;
 
-public class StarterForest {
-        private ArrayList<Pokemon> pokemonListStarterForest = new ArrayList<>();
+public class StarterForest extends Area {
 
-        public void addPokemon(Pokemon pokemon) {
-            pokemonListStarterForest.add(pokemon);
-        }
-
-        public void showAllPokemon() {
-            System.out.println("\nPokémon i skoven:");
-            for (Pokemon p : pokemonListStarterForest) {
-                System.out.println(p);
-            }
-        }
-
-        public Pokemon searchPokemonByName(String name) {
-            for (Pokemon p : pokemonListStarterForest) {
-                if (p.getName().equalsIgnoreCase(name)) {
-                    return p;
-                }
-            }
-            return null;
-        }
-
-        public Pokemon searchPokemonByType(String type) {
-            for (Pokemon p : pokemonListStarterForest) {
-                if (p.getType().equalsIgnoreCase(type)) {
-                    return p;
-                }
-            }
-            return null;
-        }
+    @Override
+    public void showAllPokemon() {
+        System.out.println("\nPokémon i starter skoven:");
+        super.showAllPokemon();
     }
+}
